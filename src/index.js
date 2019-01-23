db0de33f6e5a9891a38e2e81ea0725b1101d8c10
Rhoxio <rhoxiodbc@gmail.com>
2019-01-23 04:28:09 -0700

    removed unneeded express and server dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import express from 'express';
-import {app} from './consts';
-import dotenv from 'dotenv';
 import wrapper from './wrapper'
+import dotenv from 'dotenv';
 dotenv.load()
 
-//here routes defined
-import './routes';
+const api = new wrapper
 
-app.listen(3000, () => {
-  let api = new wrapper
-
-  api.getClasses( (body) => {
-    console.log(body)
-  })
-  console.log('ES6 application listening on port 3000!');
-});
+api.getClasses( (body) => {
+  console.log(body)
+})
--- src/routes.js
+++
@@ -1,6 +0,0 @@
-import {app} from './consts';
-import request from 'request';
-
-app.get('/', (req, res) => {
-  res.send('Hello world node.js es6 app.');
-});
